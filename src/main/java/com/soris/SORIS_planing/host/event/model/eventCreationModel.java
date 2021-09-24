662268a9d26b1aff9737edd94c45868ff5dea47e
Ravindu Anjana <80050358+Ravindu-Anjana@users.noreply.github.com>
2021-09-25 04:28:38 +0530

    restructuring files connected to Events function implementing event creation function

---
+++ src/main/java/com/soris/SORIS_planing/host/event/model/eventCreationModel.java
@@ -0,0 +1,47 @@
+package com.soris.SORIS_planing.host.event.model;
+
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.soris.SORIS_planing.dbUtil;
+
+public class eventCreationModel {
+    private String name;
+    private String description;
+    private int estimatedCost;
+    private  Connection con;
+    private int hid;
+
+    public eventCreationModel() throws SQLException, ClassNotFoundException {
+        this.con = dbUtil.initializeDatabase();
+    }
+
+    public boolean addEvent(String name, String description, int estimatedCost,int hid) {
+        try {
+            //assign variables
+            this.name = name;
+            this.description = description;
+            this.estimatedCost = estimatedCost;
+            this.hid=hid;
+            //insert into database
+            String query = String.format("INSERT INTO event (hid,name,description,estimatedcost,currentcost) VALUES" +
+                    " ('%d','%s','%s','%d','%d')", this.hid, this.name, this.description, this.estimatedCost, 0);
+            Statement stmt = this.con.createStatement();
+            int count = stmt.executeUpdate(query);
+            if(count>0){
+                return true;
+            }
+            else {
+                return false;
+            }
+
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+
+}
