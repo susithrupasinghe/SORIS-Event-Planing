a48cb7c29780edbe84bfe6bef989d7ad098bdd5d
Ravindu Anjana <80050358+Ravindu-Anjana@users.noreply.github.com>
2021-09-27 01:27:45 +0530

    adding date attribute to class and modifying relevant models to handle that

--- src/main/java/com/soris/SORIS_planing/host/event/model/eventCreationModel.java
+++ src/main/java/com/soris/SORIS_planing/host/event/model/eventCreationModel.java
@@ -4,12 +4,14 @@ package com.soris.SORIS_planing.host.event.model;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
 
 import com.soris.SORIS_planing.dbUtil;
 
 public class eventCreationModel {
     private String name;
     private String description;
+    private Date date;
     private int estimatedCost;
     private  Connection con;
     private int hid;
@@ -18,16 +20,18 @@ public class eventCreationModel {
         this.con = dbUtil.initializeDatabase();
     }
 
-    public boolean addEvent(String name, String description, int estimatedCost,int hid) {
+    public boolean addEvent(String name, String description, int estimatedCost, int hid, Date date) {
         try {
             //assign variables
             this.name = name;
             this.description = description;
             this.estimatedCost = estimatedCost;
             this.hid=hid;
+            this.date=date;
+
             //insert into database
             String query = String.format("INSERT INTO event (hid,name,description,estimatedcost,currentcost) VALUES" +
-                    " ('%d','%s','%s','%d','%d')", this.hid, this.name, this.description, this.estimatedCost, 0);
+                    "('%d','%s','%s','%d','%d')", this.hid,this.name, this.description, this.estimatedCost,0);
             Statement stmt = this.con.createStatement();
             int count = stmt.executeUpdate(query);
             if(count>0){
