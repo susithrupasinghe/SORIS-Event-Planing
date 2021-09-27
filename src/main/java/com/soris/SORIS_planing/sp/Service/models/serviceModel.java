c58e91f3d036d5654e197c0feeae5feacb675d63
E.M.Shavidini Ekanayake <77123486+EMSDEkanayake@users.noreply.github.com>
2021-09-28 02:42:44 +0530

    Added service.java file (#12)
    * completed NewService.jsp page
    
    * add nnew Service package
    
    * Edit new service page using template
    
    * Add name for select category
    
    * partially completed UpdateService.jsp page
    
    * Partially completed update part.
    
    * Partially completed update part.
    
    * Change file names.
    
    * Change static methods
    
    * Change static methods
    
    * Add basic functions of delete service.
    
    * Add basic functions of delete service.
    
    * Create service provider home.jsp page
    
    * Create service provider home.jsp page
    
    * Find exception in updateService.jsp
    
    * Find exception in updateService

--- src/main/java/com/soris/SORIS_planing/sp/Service/models/serviceModel.java
+++ src/main/java/com/soris/SORIS_planing/sp/Service/models/serviceModel.java
@@ -2,25 +2,31 @@ package com.soris.SORIS_planing.sp.Service.models;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import com.soris.SORIS_planing.dbUtil;
 
 public class serviceModel {
-    private static boolean isSuccess = false;
-    private static Connection con = null;
-    private static Statement stmt = null;
-    private static ResultSet rs = null;
+    private boolean isSuccess = false;
+    private Connection con = null;
+    private Statement stmt = null;
+    private ResultSet rs;
+
+//    public serviceModel() throws SQLException, ClassNotFoundException {
+//        Connection con = dbUtil.initializeDatabase();
+//        this.con = con;
+//    }
 
     public boolean insertService(String servicetname,String category, double price, double discount, String description) {
 //        boolean isSuccess = false;
 
         try {
-            con = com.soris.SORIS_planing.dbUtil.initializeDatabase();
+            con = dbUtil.initializeDatabase();
             stmt = con.createStatement();
 
-            String sql = "insert into service values (0,0,'" + servicetname + "', '" + category + "', '" + price + "', '" + price + "', '" + discount + "','" + description + "')";
-
+            String sql = "insert into soris.service(name,category,price,discount,description) values ('"+servicetname+"', '"+category+"', '"+price+"', '"+discount+"','"+description+"')";
             int rs = stmt.executeUpdate(sql);
 
             if(rs > 0){
@@ -37,45 +43,47 @@ public class serviceModel {
     }
 
     //service view
-//    public List<service> getServiceDetails(String spID){
-//        ArrayList<service> ser = new ArrayList<>();
-//
-//        int converspID = Integer.parseInt(spID);
-//
-//        try {
-//            con = com.soris.SORIS_planing.dbUtil.initializeDatabase();
-//            stmt = con.createStatement();
-//
-//            String sql = "SELECT sid, name, category, price, discount, description FROM service WHERE spid = '"+0+"'";
-//            rs = stmt.executeQuery(sql);
-//
-//            while (rs.next()){
-//                int sID = rs.getInt(1);
-//                String name = rs.getString(3);
-//                String category = rs.getString(4);
-//                double price = rs.getDouble(5);
-//                double discount = rs.getDouble(6);
-//                String description = rs.getString(6);
-//
-//                service s = new service(sID , name, category, price, discount, description);
-//                ser.add(s);
-//            }
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+    public List<service> getServiceDetails(String spID){
+        ArrayList<service> ser = new ArrayList<>();
+
+        int converspID = Integer.parseInt(spID);
+
+        try {
+            con = com.soris.SORIS_planing.dbUtil.initializeDatabase();
+            stmt = con.createStatement();
+
+            String sql = "SELECT sid, name, category, price, discount, description FROM service WHERE spid ='"+converspID+"'";
+            rs = stmt.executeQuery(sql);
+
+            while (rs.next()){
+                int sID = rs.getInt(1);
+                String name = rs.getString(3);
+                String category = rs.getString(4);
+                double price = rs.getDouble(5);
+                double discount = rs.getDouble(6);
+                String description = rs.getString(6);
+
+                service s = new service(sID , name, category, price, discount, description);
+                ser.add(s);
+            }
+            return ser;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
 //        return ser;
-//    }
+    }
 
     //update service
-    public boolean updateService(int sID, String name, String category, double price, double discount, String description){
-
+    public boolean updateService(int sID, String name, double price, double discount, String description){
+/*, String category*/
         try{
             con = com.soris.SORIS_planing.dbUtil.initializeDatabase();
             stmt = con.createStatement();
 
-            String sql = "UPDATE service set name = '"+name+"', category= '"+category+"', price= '"+price+"', discount= '"+discount+"', description= '"+description+"'"
-                         + "WHERE sid = 3";
+            String sql = "UPDATE service set name = '"+name+"', price= '"+price+"', discount= '"+discount+"', description= '"+description+"'"
+                         + "WHERE sid = 1";//, category= '"+category+"'
 
             int rs = stmt.executeUpdate(sql);
 
@@ -88,6 +96,32 @@ public class serviceModel {
         }catch (Exception e){
             e.printStackTrace();
         }
+        return isSuccess;
+    }
+
+    public boolean deleteService(String id){
+
+        int convertID = Integer.parseInt(id);
+
+        try{
+            con = com.soris.SORIS_planing.dbUtil.initializeDatabase();
+            stmt = con.createStatement();
+
+            String sql = "DELETE FROM service WHERE sid = '"+convertID+"'";
+
+            int rs;
+            rs = stmt.executeUpdate(sql);
+
+            if(rs > 0){
+                isSuccess = true;
+            }else{
+                isSuccess = false;
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
         return isSuccess;
     }
 
