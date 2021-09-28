47fd260b7575f7d9bd4e5c85c80f68bd846eb0c3
Ravindu Anjana <80050358+Ravindu-Anjana@users.noreply.github.com>
2021-09-28 19:41:17 +0530

    Ravindu (#13)
    * initial commit
    
    * initial commit
    
    * java classes for event
    necessary java classes for event managing function
    
    * java classes for event
    necessary java classes for event managing function
    
    * java classes for event
    necessary java classes for event managing function
    
    * restructuring files connected to Events function
    implementing event creation function
    
    * adding modal to display error message to event creation page
    validating required inputs
    configuring form to input event date
    
    * taking date input
    resolving errors in forward actions
    
    * adding date attribute to class and modifying relevant models to handle that
    
    * adding iml file

--- src/main/java/com/soris/SORIS_planing/host/event/controllers/eventDelete.java
+++ src/main/java/com/soris/SORIS_planing/host/event/controllers/eventDelete.java
@@ -1,12 +1,12 @@
-package com.soris.SORIS_planing.host.event;
+package com.soris.SORIS_planing.host.event.controllers;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "eventCreation", value = "/eventCreation")
-public class eventCreation extends HttpServlet {
+@WebServlet(name = "eventDelete", value = "/eventDelete")
+public class eventDelete extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
