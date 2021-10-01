376dad08e2e6af93b688d4cb4351cac3f1e8fd10
Ravindu Anjana <80050358+Ravindu-Anjana@users.noreply.github.com>
2021-10-01 19:20:16 +0530

    update event functions

--- src/main/java/com/soris/SORIS_planing/host/event/controllers/eventList.java
+++ src/main/java/com/soris/SORIS_planing/host/event/controllers/eventList.java
@@ -41,5 +41,6 @@ public class eventList extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+
     }
 }
