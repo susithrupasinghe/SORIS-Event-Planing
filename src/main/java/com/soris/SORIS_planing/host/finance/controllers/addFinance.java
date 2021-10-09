7c1d813f04697fb4d584a5de4c2cc81b1cb724d6
Jayasinghe Y.A.O.D <80051194+OSANDA1999@users.noreply.github.com>
2021-10-09 05:34:14 -0700

    finance (#44)
    * initial commit
    
    * partially completed viewFinance
    
    * need to add session for addfinance
    
    Co-authored-by: susithrupasinghe <susith03program@gmail.com>

--- src/main/java/com/soris/SORIS_planing/host/finance/controllers/addFinance.java
+++ src/main/java/com/soris/SORIS_planing/host/finance/controllers/addFinance.java
@@ -1,19 +1,80 @@
 package com.soris.SORIS_planing.host.finance.controllers;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import com.mysql.cj.util.SaslPrep;
+import com.soris.SORIS_planing.host.finance.models.addFinanceModel;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
 
 @WebServlet(name = "addFinance", value = "/addFinance")
 public class addFinance extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    try{
+        HttpSession session = request.getSession(false);
 
+        if(session!=null && session.getAttribute("userid") != null && session.getAttribute("role") == "host"){
+            String hid = (String) session.getAttribute("userid");
+            addFinanceModel host = new addFinanceModel();
+            HashMap<String,String> eventList = host.getEventList(hid);
+            System.out.println(eventList.size());
+            request.setAttribute("list",eventList);
+            request.getRequestDispatcher("/host-dashboard/finance/addFinance.jsp").forward(request,response);
+
+        }
+
+    } catch (SQLException | ClassNotFoundException throwables) {
+        throwables.printStackTrace();
+    }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       try{
+//        String eId = request.getParameter("eId");
+        String eId = "4";
+        String description = request.getParameter("description");
+        String amount = request.getParameter("amount");
+        String  type = request.getParameter("type");
+
+
+        boolean isTrue;
+        int convertEid = Integer.parseInt(eId);
+        double convertAmount = Double.parseDouble(amount);
+        boolean Expense = false;
+        boolean Income = false;
+
+        if(type.equals("expense") ) {
+            Expense = true;
+            Income = false;
+        } else if(type.equals("income") ) {
+            Income = true;
+            Expense = false;
+        }
+        addFinanceModel finance = new addFinanceModel();
+
+            isTrue = finance.addFinance(convertEid,description,convertAmount,Income,Expense);
+            if(isTrue ){
+                request.getRequestDispatcher("/host-dashboard/finance/financeDashboard.jsp").forward(request,response);
+            } else {
+                request.setAttribute("error","fail");
+                request.getRequestDispatcher("/host-dashboard/finance/addFinance.jsp").forward(request,response);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
