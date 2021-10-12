22b9af169774c0fe49ad7be7db06a76ead7d8955
Ravindu Anjana <80050358+Ravindu-Anjana@users.noreply.github.com>
2021-10-13 01:53:46 +0530

    added loggers

--- src/main/java/com/soris/SORIS_planing/host/event/controllers/eventCreation.java
+++ src/main/java/com/soris/SORIS_planing/host/event/controllers/eventCreation.java
@@ -10,24 +10,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 @WebServlet(name = "eventCreation", value = "/eventCreation")
 public class eventCreation extends HttpServlet {
-
+    private final static Logger LOGGER =
+            Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //if the user is logged in as host get details from view and
-        HttpSession session = request.getSession(false);
-        int hid = 0;
-
         try {
-
+            //if the user is logged in as host get parameters .
+            HttpSession session = request.getSession(false);
+            int hid = 0;
             if (session.getAttribute("userid") != null && session.getAttribute("role") == "host") {
+                LOGGER.log(Level.INFO, "User is logged as host");
                 String _hid = (String) session.getAttribute("userid");
                 hid = Integer.parseInt(_hid);
 
             } else {
+                LOGGER.log(Level.INFO, "User is not logged in");
                 //user not logged in
                 request.getRequestDispatcher("/auth/host-user-signin.jsp").forward(request, response);
             }
@@ -38,11 +41,13 @@ public class eventCreation extends HttpServlet {
             double estimatedCost = Double.parseDouble(request.getParameter("estimatedCost"));
             String date = request.getParameter("date");
 
-
+            // pass to relevant model and forward to the jsp after model returns
             eventCreationModel event = new eventCreationModel();
             if (event.addEvent(name, description, estimatedCost, hid, date)) {
+                LOGGER.log(Level.INFO, "event created");
                 response.sendRedirect(request.getContextPath() + "/eventList");
             } else {
+                LOGGER.log(Level.INFO, "event creation failed");
                 request.setAttribute("error", "event creation failed");
                 request.getRequestDispatcher("/host-dashboard/event/eventCreation.jsp").forward(request, response);
             }
