59c0d5eb0c81550f0ea5b28232c12b403e1fa5f5
Ravindu Anjana <80050358+Ravindu-Anjana@users.noreply.github.com>
2021-10-12 14:35:22 +0530

    Code Cleanup (#66)
    * initial commit
    
    * initial commit
    
    * java classes for event
    necessary java classes for event managing function
    
    * java classes for event
    necessary java classes for event managing function
    
    * java classes for event
    necessary java classes for event managing function
    
    * restructuring files connected to Events function
    implementing event creation function
    
    * adding modal to display error message to event creation page
    validating required inputs
    configuring form to input event date
    
    * taking date input
    resolving errors in forward actions
    
    * adding date attribute to class and modifying relevant models to handle that
    
    * adding iml file
    
    * entering event date
    
    * add 2 new models
    
    * Added evenList controller,event model and eventupdate jsp file
    
    * update event functions
    
    * update event functions
    
    * eventcreation,eventList,eventDeleteModel update
    
    * delete and update models
    
    * iml file
    
    * Delete Function
    
    * Delete Function
    
    * Displaying Current Income and Current cost in host-dashboard
    
    * Adding Links to left Navigation Bar
    
    * username Change in event Dashboard
    
    * Add links in left navigation bar
    
    * Optimize imports
    
    * frontend validation
    
    * navigation bar
    
    * Error on eventUpdateModel resolved
    
    * code cleanup
    
    * added read-only file

--- src/main/java/com/soris/SORIS_planing/host/event/controllers/eventUpdate.java
+++ src/main/java/com/soris/SORIS_planing/host/event/controllers/eventUpdate.java
@@ -1,28 +1,32 @@
 package com.soris.SORIS_planing.host.event.controllers;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import com.soris.SORIS_planing.host.event.model.event;
+import com.soris.SORIS_planing.host.event.model.eventUpdateModel;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
-
-import com.soris.SORIS_planing.host.event.model.*;
 
 @WebServlet(name = "eventUpdate", value = "/eventUpdate")
 public class eventUpdate extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int eid= Integer.parseInt(request.getParameter("eid"));
+        //get eid of an event from view and create the update page relevant to event
 
         try {
-            eventUpdateModel updateModel=new eventUpdateModel();
-            event event=updateModel.eventDetails(eid);
-            request.setAttribute("eid",eid);
-            request.setAttribute("date",event.getDate());
-            request.setAttribute("name",event.getName());
-            request.setAttribute("description",event.getDescription());
-            request.setAttribute("estimatedcost",event.getEstimatedCost());
-            request.getRequestDispatcher("/host-dashboard/event/eventUpdate.jsp").forward(request,response);
+            int eid = Integer.parseInt(request.getParameter("eid"));
+            eventUpdateModel updateModel = new eventUpdateModel();
+            event event = updateModel.eventDetails(eid);
+            request.setAttribute("eid", eid);
+            request.setAttribute("date", event.getDate());
+            request.setAttribute("name", event.getName());
+            request.setAttribute("description", event.getDescription());
+            request.setAttribute("estimatedcost", event.getEstimatedCost());
+            request.getRequestDispatcher("/host-dashboard/event/eventUpdate.jsp").forward(request, response);
 
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
@@ -33,20 +37,21 @@ public class eventUpdate extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // get the updated event details from view and send them to model
         try {
-            int eid= Integer.parseInt(request.getParameter("eid"));
-            String name=request.getParameter("name");
-            String date=request.getParameter("date");
-            String description=request.getParameter("description");
-            double estimatedCost= Double.parseDouble(request.getParameter("estimatedCost"));
+            int eid = Integer.parseInt(request.getParameter("eid"));
+            String name = request.getParameter("name");
+            String date = request.getParameter("date");
+            String description = request.getParameter("description");
+            double estimatedCost = Double.parseDouble(request.getParameter("estimatedCost"));
 
 
-            eventUpdateModel updateModel= new eventUpdateModel();
-            if(updateModel.eventUpdate(eid,date,name,description,estimatedCost)){
+            eventUpdateModel updateModel = new eventUpdateModel();
+            if (updateModel.eventUpdate(eid, date, name, description, estimatedCost)) {
                 response.sendRedirect(request.getContextPath() + "/eventList");
-            }else {
-                request.setAttribute("error","event update failed");
-                request.getRequestDispatcher("/host-dashboard/event/eventUpdate.jsp").forward(request,response);
+            } else {
+                request.setAttribute("error", "event update failed");
+                request.getRequestDispatcher("/host-dashboard/event/eventUpdate.jsp").forward(request, response);
             }
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
